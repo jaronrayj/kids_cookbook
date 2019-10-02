116054d149d3f30816b61cdff40fb967e48b6f19
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-10-01 23:22:28 -0600

    Added hashes to url

--- src/App.js
+++ src/App.js
@@ -7,39 +7,39 @@ class App extends Component {
     foods = [
         {
             name: "Pancakes",
-            link: "/kids_cookbook/pancakes",
+            link: "/kids_cookbook/#/pancakes",
             img: "https://img.icons8.com/officel/480/000000/pancake.png"
         }, {
             name: "Pizza",
-            link: "/kids_cookbook/pizza",
+            link: "/kids_cookbook/#/pizza",
             img: "https://img.icons8.com/officel/480/000000/pizza.png"
         }, {
             name: "Smoothie",
-            link: "/kids_cookbook/smoothie",
+            link: "/kids_cookbook/#/smoothie",
             img: "https://img.icons8.com/color/480/000000/milkshake--v2.png"
         }, {
             name: "Biscuits",
-            link: "/kids_cookbook/biscuits",
+            link: "/kids_cookbook/#/biscuits",
             img: "https://img.icons8.com/dusk/480/000000/biscuits.png"
         }, {
             name: "Cookies",
-            link: "/kids_cookbook/cookies",
+            link: "/kids_cookbook/#/cookies",
             img: "https://img.icons8.com/plasticine/480/000000/cookie.png"
         }, {
             name: "Rice",
-            link: "/kids_cookbook/rice",
+            link: "/kids_cookbook/#/rice",
             img: "https://img.icons8.com/plasticine/480/000000/rice-bowl.png"
         }, {
             name: "Spaghetti",
-            link: "/kids_cookbook/spaghetti",
+            link: "/kids_cookbook/#/spaghetti",
             img: "https://img.icons8.com/color/480/000000/spaghetti.png"
         }, {
             name: "Tacos",
-            link: "/kids_cookbook/tacos",
+            link: "/kids_cookbook/#/tacos",
             img: "https://img.icons8.com/plasticine/480/000000/taco.png"
         }, {
             name: "Peanut Butter and Jelly",
-            link: "/kids_cookbook/pbandj",
+            link: "/kids_cookbook/#/pbandj",
             img: "https://img.icons8.com/color/480/000000/sandwich.png"
         }
     ]
