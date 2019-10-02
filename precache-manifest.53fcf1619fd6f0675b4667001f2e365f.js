40b6ae85e76cd53354d6316802d5c2f07b7c48cd
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-10-01 23:20:26 -0600

    Updates

---
+++ precache-manifest.53fcf1619fd6f0675b4667001f2e365f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05124ca6bdb4555b8a080af36078e1c",
+    "url": "/kids_cookbook/index.html"
+  },
+  {
+    "revision": "d44c46a5df92d8224bf4",
+    "url": "/kids_cookbook/static/css/main.5c8dcc41.chunk.css"
+  },
+  {
+    "revision": "cb7958d4017eaff03de2",
+    "url": "/kids_cookbook/static/js/2.dad13953.chunk.js"
+  },
+  {
+    "revision": "d44c46a5df92d8224bf4",
+    "url": "/kids_cookbook/static/js/main.a50b4b2f.chunk.js"
+  },
+  {
+    "revision": "f4ee052083e22853fb42",
+    "url": "/kids_cookbook/static/js/runtime-main.b3d4e3d0.js"
+  }
+]);
