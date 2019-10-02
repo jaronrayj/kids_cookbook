61d94d186d00e611b5040510e19dc49a8e377c41
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-10-01 22:50:46 -0600

    Updated images for pizza

--- src/components/Pizza/index.js
+++ src/components/Pizza/index.js
@@ -1,22 +1,76 @@
 import React, {Component} from 'react'
-import { Header, Progress, Divider, Image } from 'semantic-ui-react';
+import { Header, Progress, Divider, Image, Button } from 'semantic-ui-react';
 
 export default function Pizza (){
 
 
         return(
             <>
+                <Progress percent="20" indicating />
                 <Header size='large'>
                     Pizza, Pizza, Pizza!!
                 </Header>
                 <Divider />
-                <Progress fixed percent="20" indicating />
+                <Header>
+                    1. Oven
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/cooker.png' /> 
+                <Button size='huge' onClick>Done!</Button>
                 <Divider />
-                <span>
-                    Dough
-                </span>
-                <Image className="lefty" src='https://img.icons8.com/officel/150/000000/bread.png' /> 
-                <Image centered src='https://img.icons8.com/officel/120/000000/pizza.png' />
+                <Header>
+                    2. Dough
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/bread.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    3. Sauce
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/ketchup.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    4. Cheese
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/cheese.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    5. Bacon
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/bacon.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    6. Tomato
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/tomato.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    7. Anything Else?
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/question-mark.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    8. Cook!
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/cooker.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    9. Cut
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/200/000000/pizza-cutter--v2.png' /> 
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
+                <Header>
+                    10. Eat
+                </Header>
+                <Image centered src='https://img.icons8.com/officel/300/000000/pizza.png' />
+                <Button size='huge' onClick>Done!</Button>
+                <Divider />
             </>
         );
     }
