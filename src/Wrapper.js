c1692c24bc3afb409ad4e062eb553bc9e29e9be2
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-10-15 22:40:07 -0600

    updated menu with color and new text

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Container, Menu } from 'semantic-ui-react'
 import { Route, Switch, Link } from "react-router-dom";
 import Pizza from './components/Pizza'
@@ -10,37 +10,37 @@ import PBandJ from './components/PBandJ'
 import App from './App'
 import './style.css'
 
-class Wrapper extends Component {
-    render() {
-        return (
-            <>
-                <Menu size='huge' fixed='top'>
-                    <Link to='/'>
-                        <Menu.Item name='Home'>
-                        </Menu.Item>
-                    </Link>
-                </Menu>
-                <Menu secondary>
-                    <Menu.Item position='right' name='Icons by Icon8' href='https://icons8.com'>
-                    </Menu.Item>
-                </Menu>
-                <Container >
-                    <Switch>
-                        <Route exact path="/" component={App} />
-                        <Route exact path="/pizza" component={Pizza} />
-                        <Route path="/biscuits" component={Biscuit} />
-                        <Route path="/smoothie" component={FruitSmoothie} />
-                        <Route path="/rice" component={Rice}/>
-                        <Route path="/pancakes" component={Pancake}/>
-                        <Route path="/pbandj" component={PBandJ}/>
-                        {/* <Route path="/Pancakes" component={Pancakes}/>
+function Wrapper() {
+
+    return (
+        <>
+            <Menu inverted color='teal' size='huge' fixed='top'>
+                <Link to='/'>
+                    <Menu.Item active header>Kids Cookbook</Menu.Item>
+                </Link>
+                {/* <Menu.Item size='large' position='right' >Kids Cookbook</Menu.Item> */}
+            </Menu>
+            <Menu secondary >
+                <Menu.Item position='right' name='Icons by Icon8' href='https://icons8.com'>
+                </Menu.Item>
+            </Menu>
+            <Container >
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route exact path="/pizza" component={Pizza} />
+                    <Route path="/biscuits" component={Biscuit} />
+                    <Route path="/smoothie" component={FruitSmoothie} />
+                    <Route path="/rice" component={Rice} />
+                    <Route path="/pancakes" component={Pancake} />
+                    <Route path="/pbandj" component={PBandJ} />
+                    {/* <Route path="/Pancakes" component={Pancakes}/>
                 <Route path="/ChocSmoothie" component={ChocSmoothie}/>
                 <Route path="/Blank8" component={Blank8}/> */}
-                    </Switch>
-                </Container>
-            </>
-        )
-    }
+                </Switch>
+            </Container>
+        </>
+    )
+
 }
 
 export default Wrapper
